6b06553a6eef2816bdf9d1a328c4d177ac6ebf06
Elcio <elcio.hintze@ndd.com.br>
2019-07-04 18:16:35 -0300

    dasdas

--- OrganizarBackup/Apresentacao/WFSettings.cs
+++ OrganizarBackup/Apresentacao/WFSettings.cs
@@ -125,7 +125,7 @@ namespace OrganizarBackup
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                txtInput.Text = fbd.SelectedPath;//selecionado diretorio
+                txtInput.Text = fbd.SelectedPath;//selecionado diretorio sss
             }
         }
 
